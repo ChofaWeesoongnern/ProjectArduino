ebbf815918ea9aa2385e4d0275fcf4af986a31c6
ChofaWeesoongnern <115547276+ChofaWeesoongnern@users.noreply.github.com>
2023-04-25 12:14:17 +0700

    Add files via upload

--- project.ino
+++ project.ino
@@ -1,148 +1,127 @@
-#include <CapacitiveSensor.h>
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 
-LiquidCrystal_I2C lcd(0x3F,16,2);
-CapacitiveSensor capSensor  = CapacitiveSensor(4, 2);
-CapacitiveSensor capSensor2 = CapacitiveSensor(4, 5);
-CapacitiveSensor capSensor5 = CapacitiveSensor(4, 6);
-CapacitiveSensor capSensor10 = CapacitiveSensor(4, 7);
+LiquidCrystal_I2C lcd(0x27,16,2);
 
-int ms_time   = 800;
-int threshold = 640;
-const int ledPin = 13;
-bool led_state = LOW;
-bool pass = false;
+int pinSpeed_1 = 13;
+int pinSpeed_2 = 12;
+int pinSpeed_3 = 11;
+int pinSpeed_4 = 10 ;
 
-int count_c1 = 0;
-int count_c2 = 0;
-int count_c5 = 0;
-int count_c10 = 0;
-int sum       = 0;
+int state_Speed_1 = 0;
+int state_Speed_2 = 0;
+int state_Speed_3 = 0;
+int state_Speed_4 = 0;
 
-unsigned long ms1=0;
-unsigned long ms2=0;
-unsigned long ms5=0;
-unsigned long ms10=0;
+int coin_10 = 0 ;
+int coin_5 = 0 ;
+int coin_2 = 0 ;
+int coin_1 = 0 ;
+int sum = 0;
+
+int oneClink_1 = 0;
+int oneClink_2 = 0;
+int oneClink_3 = 0;
+int oneClink_4 = 0;
 
 void setup()
 {
-  lcd.init();                      // initialize the lcd
+  lcd.init();     // initialize the lcd
   // Print a message to the LCD.
   lcd.backlight();
   lcd.setCursor(0,0);
   lcd.print("1  2  5  10  sum");
+
   Serial.begin(9600);
-  pinMode(ledPin, OUTPUT);
   delay(50);
+  pinMode(pinSpeed_1, INPUT); 
+  pinMode(pinSpeed_2, INPUT); 
+  pinMode(pinSpeed_3, INPUT);
+  pinMode(pinSpeed_4, INPUT);
 }
 
 void loop()
 {
-   checkCoin1();
-   checkCoin2();
-   checkCoin5();
-   checkCoin10();
-   lcd.clear();
-   lcd.setCursor(0,1);
-   lcd.print( count_c1);
-   lcd.setCursor(3,1);
-   lcd.print( count_c2);
-   lcd.setCursor(6,1);
-   lcd.print( count_c5);
-   lcd.setCursor(9,1);
-   lcd.print( count_c10);
-   lcd.setCursor(13,1);
-   lcd.print( sum);
 
-}
+  state_Speed_1 = digitalRead(pinSpeed_1);
+  state_Speed_2 = digitalRead(pinSpeed_2);
+  state_Speed_3 = digitalRead(pinSpeed_3);
+  state_Speed_4 = digitalRead(pinSpeed_4);
+  
+  Serial.println(state_Speed_1);
+  Serial.println(state_Speed_2);
+  Serial.println(state_Speed_3);
+  Serial.println(state_Speed_4);
 
-void checkCoin1()
-{
-  long sensorValue = capSensor.capacitiveSensor(30);
-  Serial.println(sensorValue);
-  if (sensorValue > threshold)
-  {
-    if( ms1 == 0){
-      ms1 = millis();
-      count_c1++;
+  if (state_Speed_1 == 1) {
+    if (oneClink_1 == 0) {
+      oneClink_1 = 1;
+      coin_1++;
       sum = sum + 1;
     }
-
-      digitalWrite(ledPin, HIGH);
-  }else{
-      digitalWrite(ledPin, LOW);
   }
-  if( ms1 != 0){
-      if( (millis() - ms1) > ms_time){
-         ms1 = 0;
-      }
+  else if (state_Speed_1 == 0 ) {
+    oneClink_1 = 0;
   }
-}
 
-void checkCoin2()
-{
-  long sensorValue = capSensor2.capacitiveSensor(30);
-  Serial.println(sensorValue);
-  if (sensorValue > threshold)
-  {
-    if( ms2 == 0){
-      ms2 = millis();
-      count_c2++;
+
+
+  if (state_Speed_2 == 1) {
+    if (oneClink_2 == 0) {
+      oneClink_2 = 1;
+      coin_2++;
       sum = sum + 2;
     }
-      digitalWrite(ledPin, HIGH);
-  }else{
-      digitalWrite(ledPin, LOW);
   }
-  if( ms2 != 0){
-      if( (millis() - ms2) > ms_time){
-         ms2 = 0;
-      }
+  else if (state_Speed_2 == 0 ) {
+    oneClink_2 = 0;
   }
-}
 
-void checkCoin5()
-{
-  long sensorValue = capSensor5.capacitiveSensor(30);
-  Serial.println(sensorValue);
-  if (sensorValue > threshold)
-  {
-    if( ms5 == 0){
-      ms5 = millis();
-      count_c5++;
+
+
+  if (state_Speed_3 == 1) {
+    if (oneClink_3 == 0) {
+      oneClink_3 = 1;
+      coin_5 ++;
       sum = sum + 5;
     }
-      digitalWrite(ledPin, HIGH);
-  }else{
-      digitalWrite(ledPin, LOW);
   }
-  if( ms5 != 0){
-      if( (millis() - ms5) > ms_time){
-         ms5 = 0;
-      }
+  else if (state_Speed_3 == 0 ) {
+    oneClink_3 = 0;
   }
-}
 
-void checkCoin10()
 
-{
-  long sensorValue = capSensor10.capacitiveSensor(30);
-  Serial.println(sensorValue);
-  if (sensorValue > threshold)
-  {
-    if( ms10 == 0){
-      ms10 = millis();
-      count_c10++;
+if (state_Speed_4 == 1) {
+    if (oneClink_4 == 0) {
+      oneClink_4 = 1;
+      coin_10 ++;
       sum = sum + 10;
     }
-      digitalWrite(ledPin, HIGH);
-  }else{
-      digitalWrite(ledPin, LOW);
   }
-  if( ms10 != 0){
-      if( (millis() - ms10) > ms_time){
-         ms10 = 0;
-      }
+  else if (state_Speed_4 == 0 ) {
+    oneClink_4 = 0;
   }
+
+  Serial.println(coin_1);
+  Serial.println(coin_2);
+  Serial.println(coin_5);
+  Serial.println(coin_10);
+
+
+  lcd.setCursor(0, 1);
+  lcd.print(coin_1);
+
+  lcd.setCursor(3, 1);
+  lcd.print(coin_2);
+
+  lcd.setCursor(6, 1);
+  lcd.print(coin_5);
+
+  lcd.setCursor(9, 1);
+  lcd.print(coin_10);
+
+  lcd.setCursor(13,1);
+  lcd.print(sum);
+
+
 }
